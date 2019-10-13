fa183352fcd1cadad9ff8e06e28bd18c15f77316
Luis F H Serrano <luisfelipehsr@gmail.com>
2019-10-12 23:52:39 -0300

    new scripts to run pneumonia

--- scripts/create_jpeg_dataset.py
+++ scripts/create_jpeg_dataset.py
@@ -5,7 +5,7 @@ import sys
 from PIL import Image
 
 # make it True if you want in PNG format
-PNG = False
+PNG = True
 # Specify the .dcm folder path
 folder_path = sys.argv[1]
 # Specify the output jpg/png folder path
@@ -18,7 +18,7 @@ images_path = os.listdir(folder_path)
 for n, image in enumerate(images_path):
     ds = dicom.dcmread(os.path.join(folder_path, image))
     pixel_array_numpy = ds.pixel_array
-    if PNG == False:
+    if PNG is False:
         image = image.replace('.dcm', '.jpg')
     else:
         image = image.replace('.dcm', '.png')
