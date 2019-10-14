cf1adbdfc5555cb82f54e6ca977a35b10807ffce
Luis F H Serrano <luisfelipehsr@gmail.com>
2019-10-13 23:07:14 -0300

    fixing typo in scripts

--- run_pneumonia_standard.sh
+++ run_pneumonia_standard.sh
@@ -2,10 +2,10 @@
 DEVICE=1
 for i in 0 1 2 3 4 5 6 7 8 9
 do
-    python3 train_sacred_csv.py with train_root=pneumonia-rgb train_csv="pneumonia-csv/10fold/pneumonia-train-${i}.csv" device=${DEVICE} id_col='patiendId' class_col='Target' --name sacred-pneumonia-rgb-7030-nodup
+    python3 train_sacred_csv.py with train_root=pneumonia-rgb train_csv="pneumonia-csv/10fold/pneumonia-train-${i}.csv" device=${DEVICE} id_col='patientId' class_col='Target' --name sacred-pneumonia-rgb-7030-nodup
 done
 
 for i in 0 1 2 3 4 5 6 7 8 9
 do
-    python3 test_csv.py "results-sacred/$((361 + i))/checkpoints/model_best.pth" pneumonia-rgb/ "pneumonia-csv/10fold/pneumonia-test-${i}.csv" device=${DEVICE} --id_field 'patiendId' --target_field 'Target' -n 50 -p > "results-sacred/$((361 + i))/auc_test_best_all.txt"
+    python3 test_csv.py "results-sacred/$((361 + i))/checkpoints/model_best.pth" pneumonia-rgb/ "pneumonia-csv/10fold/pneumonia-test-${i}.csv" device=${DEVICE} --id_field 'patientId' --target_field 'Target' -n 50 -p > "results-sacred/$((361 + i))/auc_test_best_all.txt"
 done
