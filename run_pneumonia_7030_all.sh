04108af5163b837e84e87c42c96d356709b3063c
Luis F H Serrano <luisfelipehsr@gmail.com>
2019-10-13 22:59:56 -0300

    updating scripts to specify correct image name and label columns

--- run_pneumonia_7030_all.sh
+++ run_pneumonia_7030_all.sh
@@ -2,48 +2,48 @@
 DEVICE=1
 for i in 0 1 2 3 4 5 6 7 8 9
 do
-    python3 train_sacred_csv.py with train_root=pneumonia-rgb train_csv="pneumonia-csv/10fold/pneumonia-train-${i}.csv" device=${DEVICE} id_col='patiendId' label_col='Target' --name sacred-pneumonia-rgb-7030-nodup
+    python3 train_sacred_csv.py with train_root=pneumonia-rgb train_csv="pneumonia-csv/10fold/pneumonia-train-${i}.csv" device=${DEVICE} id_col='patiendId' class_col='Target' --name sacred-pneumonia-rgb-7030-nodup
 done
 
 for i in 0 1 2 3 4 5 6 7 8 9
 do
-    python3 test_csv.py "results-sacred/$((361 + i))/checkpoints/model_best.pth" pneumonia-rgb/ "pneumonia-csv/10fold/pneumonia-test-${i}.csv" device=${DEVICE} --id_field 'patiendId' --label_field 'Target' -n 50 -p > "results-sacred/$((361 + i))/auc_test_best_all.txt"
+    python3 test_csv.py "results-sacred/$((361 + i))/checkpoints/model_best.pth" pneumonia-rgb/ "pneumonia-csv/10fold/pneumonia-test-${i}.csv" device=${DEVICE} --id_field 'patiendId' --target_field 'Target' -n 50 -p > "results-sacred/$((361 + i))/auc_test_best_all.txt"
 done
 
 # BACKGROUND
 for i in 0 1 2 3 4 5 6 7 8 9
 do
-    python3 train_sacred_csv.py with train_root=pneumonia-background train_csv="pneumonia-csv/10fold/pneumonia-train-${i}.csv" device=${DEVICE} id_col='patiendId' label_col='Target' --name sacred-pneumonia-background-7030-nodup
+    python3 train_sacred_csv.py with train_root=pneumonia-background train_csv="pneumonia-csv/10fold/pneumonia-train-${i}.csv" device=${DEVICE} id_col='patiendId' class_col='Target' --name sacred-pneumonia-background-7030-nodup
 done
 
 for i in 0 1 2 3 4 5 6 7 8 9
 do
-    python3 test_csv.py "results-sacred/$((371 + i))/checkpoints/model_best.pth" pneumonia-background/ "pneumonia-csv/10fold/pneumonia-test-${i}.csv" device=${DEVICE} --id_field 'patiendId' --label_field 'Target' -n 50 -p > "results-sacred/$((371 + i))/auc_test_best_all.txt"
+    python3 test_csv.py "results-sacred/$((371 + i))/checkpoints/model_best.pth" pneumonia-background/ "pneumonia-csv/10fold/pneumonia-test-${i}.csv" device=${DEVICE} --id_field 'patiendId' --target_field 'Target' -n 50 -p > "results-sacred/$((371 + i))/auc_test_best_all.txt"
 done
 
 # BBOX
 for i in 0 1 2 3 4 5 6 7 8 9
 do
-    python3 train_sacred_csv.py with train_root=pneumonia-bbox train_csv="pneumonia-csv/10fold/pneumonia-train-${i}.csv" device=${DEVICE} id_col='patiendId' label_col='Target' --name sacred-pneumonia-bbox-7030-nodup
+    python3 train_sacred_csv.py with train_root=pneumonia-bbox train_csv="pneumonia-csv/10fold/pneumonia-train-${i}.csv" device=${DEVICE} id_col='patiendId' class_col='Target' --name sacred-pneumonia-bbox-7030-nodup
 done
 
 for i in 0 1 2 3 4 5 6 7 8 9
 do
-    python3 test_csv.py "results-sacred/$((381 + i))/checkpoints/model_best.pth" pneumonia-bbox/ "pneumonia-csv/10fold/pneumonia-test-${i}.csv" device=${DEVICE} --id_field 'patiendId' --label_field 'Target' -n 50 -p > "results-sacred/$((381 + i))/auc_test_best_all.txt"
+    python3 test_csv.py "results-sacred/$((381 + i))/checkpoints/model_best.pth" pneumonia-bbox/ "pneumonia-csv/10fold/pneumonia-test-${i}.csv" device=${DEVICE} --id_field 'patiendId' --target_field 'Target' -n 50 -p > "results-sacred/$((381 + i))/auc_test_best_all.txt"
 done
 
 # BBOX 70
 for i in 0 1 2 3 4 5 6 7 8 9
 do
-    python3 train_sacred_csv.py with train_root=pneumonia-bbox-squared-70 "train_csv=pneumonia-csv/10fold/pneumonia-train-${i}.csv" device=${DEVICE} id_col='patiendId' label_col='Target' --name sacred-pneumonia-bbox-squared-70-7030-nodup
+    python3 train_sacred_csv.py with train_root=pneumonia-bbox-squared-70 "train_csv=pneumonia-csv/10fold/pneumonia-train-${i}.csv" device=${DEVICE} id_col='patiendId' class_col='Target' --name sacred-pneumonia-bbox-squared-70-7030-nodup
 done
 
 for i in 0 1 2 3 4 5 6 7 8 9
 do
-    python3 test_csv.py "results-sacred/$((391 + i))/checkpoints/model_best.pth" pneumonia-bbox-squared-70/ "pneumonia-csv/10fold/pneumonia-test-${i}.csv" device=${DEVICE} --id_field 'patiendId' --label_field 'Target' -n 50 -p > "results-sacred/$((391 + i))/auc_test_best_all.txt"
+    python3 test_csv.py "results-sacred/$((391 + i))/checkpoints/model_best.pth" pneumonia-bbox-squared-70/ "pneumonia-csv/10fold/pneumonia-test-${i}.csv" device=${DEVICE} --id_field 'patiendId' --target_field 'Target' -n 50 -p > "results-sacred/$((391 + i))/auc_test_best_all.txt"
 done
 
 for i in 0 1 2 3 4 5 6 7 8 9
 do
-    python3 train_sacred_csv.py with train_root=pneumonia-label "train_csv=pneumonia-csv/10fold/pneumonia-train-${i}.csv" device=${DEVICE} id_col='patiendId' label_col='Target' --name sacred-pneumonia-label-7030-nodup
+    python3 train_sacred_csv.py with train_root=pneumonia-label "train_csv=pneumonia-csv/10fold/pneumonia-train-${i}.csv" device=${DEVICE} id_col='patiendId' class_col='Target' --name sacred-pneumonia-label-7030-nodup
 done
