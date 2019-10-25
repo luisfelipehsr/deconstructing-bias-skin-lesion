5e169b59a43978a68422c8108cdfc1d4d1161719
Luis F H Serrano <luisfelipehsr@gmail.com>
2019-10-25 01:09:37 -0300

    adding script just for pneumonia test

---
+++ run_pneumonia_test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+DEVICE=1
+BASE=1
+for i in 0 1 2 3 4 5 6 7 8 9
+do
+    python3 test_csv.py "results-sacred/$((BASE + i))/checkpoints/model_best.pth" pneumonia-rgb/ "pneumonia-csv/10fold/pneumonia-test-${i}.csv" --device ${DEVICE} --id_field 'patientId' --target_field 'Target' -n 50 -p > "results-sacred/$((BASE + i))/auc_test_best_all.txt"
+done
