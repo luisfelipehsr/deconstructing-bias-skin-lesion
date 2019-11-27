5cd1a4569bc0a6780ebb0825ebfc02c063d8fd87
Luis F H Serrano <luisfelipe@neuralmind.ai>
2019-11-27 14:42:37 -0300

    updating scripts

--- run_pneumonia_bias.sh
+++ run_pneumonia_bias.sh
@@ -6,11 +6,11 @@ TRAIN_DIR='pneumonia-masks'
 # train
 for i in 0 1 2 3 4 5 6 7 8 9
 do
-    python3 train_sacred_csv.py with train_root="${TRAIN_DIR}" train_csv="pneumonia-csv/10fold-classication/pneumonia-train-${i}.csv" device=${DEVICE} id_col='patientId' class_col='Target' --name sacred-pneumonia-rgb-7030-nodup
+    python3 train_sacred_csv.py with train_root="${TRAIN_DIR}" train_csv="pneumonia-csv/10fold-classification-classication/pneumonia-train-${i}.csv" device=${DEVICE} id_col='patientId' class_col='Target' --name sacred-pneumonia-rgb-7030-nodup
 done
 
 # test
 for i in 0 1 2 3 4 5 6 7 8 9
 do
-    python3 test_csv.py "results-sacred/$((BASE + i))/checkpoints/model_best.pth" "${TRAIN_DIR}" "pneumonia-csv/10fold-classication/pneumonia-test-${i}.csv" --device ${DEVICE} --id_field 'patientId' --target_field 'Target' -n 50 -p > "results-sacred/$((BASE + i))/auc_test_best_all.txt"
+    python3 test_csv.py "results-sacred/$((BASE + i))/checkpoints/model_best.pth" "${TRAIN_DIR}" "pneumonia-csv/10fold-classification-classication/pneumonia-test-${i}.csv" --device ${DEVICE} --id_field 'patientId' --target_field 'Target' -n 50 -p > "results-sacred/$((BASE + i))/auc_test_best_all.txt"
 done
