906ae238410891ddd097f32bf33f3662a996f31e
Luis F H Serrano <luisfelipehsr@gmail.com>
2019-12-14 22:19:23 -0300

    fixing last script

--- run_pneumonia_center_70b.sh
+++ run_pneumonia_center_70b.sh
@@ -8,15 +8,15 @@ CSV_DIR='pneumonia/ratio'
 for i in 0 1 2 3
 do
     python3 train_sacred_csv.py with train_root="${TRAIN_DIR}/0.7" train_csv="${CSV_DIR}/0.7/10fold-classification/pneumonia-train-$((i + 6)).csv" device=${DEVICE} id_col='patientId' class_col='Target' --name "sacred-pneumonia-rgb-0.7-$((i + 6))"
-    python3 test_csv.py "results-sacred/$((BASE + i * 3))/checkpoints/model_best.pth" "${TRAIN_DIR}/0.7" "${CSV_DIR}/0.7/10fold-classification/pneumonia-test-$((i + 6)).csv" --device ${DEVICE} --id_field 'patientId' --target_field 'Target' -n 50 -p > "results-sacred/$((BASE + i * 3))/test_best_all.txt"
+    python3 test_csv.py "results-sacred/$((BASE + i * 2))/checkpoints/model_best.pth" "${TRAIN_DIR}/0.7" "${CSV_DIR}/0.7/10fold-classification/pneumonia-test-$((i + 6)).csv" --device ${DEVICE} --id_field 'patientId' --target_field 'Target' -n 50 -p > "results-sacred/$((BASE + i * 2))/test_best_all.txt"
 
     python3 train_sacred_csv.py with train_root="${TRAIN_DIR}/0.9" train_csv="${CSV_DIR}/0.7/10fold-classification/pneumonia-train-$((i + 6)).csv" device=${DEVICE} id_col='patientId' class_col='Target' --name "sacred-pneumonia-rgb-0.9-$((i + 6))"
-    python3 test_csv.py "results-sacred/$((BASE + 2 + i * 3))/checkpoints/model_best.pth" "${TRAIN_DIR}/0.9" "${CSV_DIR}/0.7/10fold-classification/pneumonia-test-$((i + 6)).csv" --device ${DEVICE} --id_field 'patientId' --target_field 'Target' -n 50 -p > "results-sacred/$((BASE + 2 + i * 3))/test_best_all.txt"
+    python3 test_csv.py "results-sacred/$((BASE + 1 + i * 2))/checkpoints/model_best.pth" "${TRAIN_DIR}/0.9" "${CSV_DIR}/0.7/10fold-classification/pneumonia-test-$((i + 6)).csv" --device ${DEVICE} --id_field 'patientId' --target_field 'Target' -n 50 -p > "results-sacred/$((BASE + 1 + i * 2))/test_best_all.txt"
 done
 
 for i in 0 1 2 3
 do
 
     python3 train_sacred_csv.py with train_root="${TRAIN_DIR}/0.8" train_csv="${CSV_DIR}/0.7/10fold-classification/pneumonia-train-$((i + 6)).csv" device=${DEVICE} id_col='patientId' class_col='Target' --name "sacred-pneumonia-rgb-0.8-$((i + 6))"
-    python3 test_csv.py "results-sacred/$((BASE + 1 + i * 3))/checkpoints/model_best.pth" "${TRAIN_DIR}/0.8" "${CSV_DIR}/0.7/10fold-classification/pneumonia-test-$((i + 6)).csv" --device ${DEVICE} --id_field 'patientId' --target_field 'Target' -n 50 -p > "results-sacred/$((BASE + 1 + i * 3))/test_best_all.txt"
+    python3 test_csv.py "results-sacred/$((BASE + 8 + i))/checkpoints/model_best.pth" "${TRAIN_DIR}/0.8" "${CSV_DIR}/0.7/10fold-classification/pneumonia-test-$((i + 6)).csv" --device ${DEVICE} --id_field 'patientId' --target_field 'Target' -n 50 -p > "results-sacred/$((BASE + 8 + i))/test_best_all.txt"
 done
